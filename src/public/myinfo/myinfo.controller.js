79bb4db2c071300cf54f2a338907dd82c195b3d1
jinrokjack <jacekbomber@gmail.com>
2017-03-09 13:10:29 +0100

    Update myinfo.controller.js

--- src/public/myinfo/myinfo.controller.js
+++ src/public/myinfo/myinfo.controller.js
@@ -4,21 +4,10 @@
 angular.module('public')
 .controller('MyInfoController', MyInfoController);
 
-MyInfoController.$inject = ['MenuService', 'info'];
-function MyInfoController(MenuService, info) {
-  var $ctrl = this;
-
-  if (info) {
-    $ctrl.info = info;
-    MenuService.getMenuItem(info.favorite)
-      .then(function(response) {
-        $ctrl.menuItem = response;
-      })
-      .catch(function(response) {
-        console.log(response);
-      });
-  }
-};
+MyInfoController.$inject = ['userInfo'];
+function MyInfoController(userInfo) {
+  var $myInfoCtrl = this;
+  $myInfoCtrl.user = userInfo;
+}
 
 })();
-
